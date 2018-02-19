7b223949bc3a193257092aa4faf124b618461a8b
tarunkmr145 <tarun.codedrill@gmail.com>
2018-02-19 13:20:50 +0530

    Migrations created for product and reviews

--- database/migrations/2018_02_19_054525_create_products_table.php
+++ database/migrations/2018_02_19_054525_create_products_table.php
@@ -15,6 +15,11 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->text('detail');
+            $table->string('price');
+            $table->string('stock');
+            $table->string('discount');
             $table->timestamps();
         });
     }
