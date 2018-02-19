73fb1a719f46bb47429dd4194ca7c5727bfefce4
tarunkmr145 <tarun.codedrill@gmail.com>
2018-02-19 17:39:52 +0530

    Modify product details

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Model\Review;
 use Illuminate\Http\Request;
+use App\Http\Resources\Product\ProductResource;
 
 class ReviewController extends Controller
 {
